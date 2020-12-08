556833a49c72abbc6aadd01f2577efcb144b1136
thashans <62001103+thashans@users.noreply.github.com>
2020-12-08 20:15:48 -0300

    pirmeiras linhas para pratica

---
+++ main.js
@@ -0,0 +1,14 @@
+/* Para colocor comntários use /*, para fechar*/
+//var nome = "Natacha Araujo";
+//var num1 = 8; /* Se colocarr "", entende como texto*/
+//var num2 = 15;
+//var frase = " Japão é o melhor time do mundo";
+//alert("Bem Vinda " + nome);
+//alert( nome + " tem " + idade + " anos ")
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(num1 * num2);
+var lista = ["uva", "maracuja", "caju"]; /* para dar certo tem que colocar []*/
+//lista.push ("uva");
+//lista.pop("caju");
+console.log(lista);
